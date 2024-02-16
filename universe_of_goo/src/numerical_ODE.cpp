e208e684eded56ba414c36b20a370d191b4e2055
qing42102 <qing42102@outlook.com>
2024-02-15 22:51:51 -0600

    Add floor force.

--- universe_of_goo/src/numerical_ODE.cpp
+++ universe_of_goo/src/numerical_ODE.cpp
@@ -29,21 +29,19 @@ void computeMassInverse(Eigen::SparseMatrix<double> &Minv)
 /*
     Add the force due to gravity to the force vector F
     Add the Hessian of the gravity force to the Hessian matrix H
-    V = \sum_i m_i * g * y_i
+    V = -\sum_i m_i * g * y_i
     F = [0, \sum_i m_i * g]
     H = [0, 0, 0, 0]
 */
-void compute_gravity_force(const Eigen::VectorXd &q, const Eigen::VectorXd &qprev, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
+void compute_gravity_force(const Eigen::VectorXd &q, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
 {
-    if (params_.gravityEnabled)
+
+    for (uint i = 0; i < particles_.size(); i++)
     {
-        for (uint i = 0; i < particles_.size(); i++)
+        if (!particles_[i].fixed)
         {
-            if (!particles_[i].fixed)
-            {
-                F(2 * i) += 0;
-                F(2 * i + 1) += params_.gravityG * particles_[i].mass;
-            }
+            F(2 * i) += 0;
+            F(2 * i + 1) += params_.gravityG * particles_[i].mass;
         }
     }
 }
@@ -55,61 +53,77 @@ void compute_gravity_force(const Eigen::VectorXd &q, const Eigen::VectorXd &qpre
     F = \sum_{ij} -k_{ij} (\Vert p_i - p_j \Vert - L_{ij}) \frac{p_i - p_j}{\Vert p_i - p_j \Vert}
     H = \sum_{ij} k_{ij} \left(\frac{(p_i - p_j) (p_i - p_j)^T}{\Vert p_i - p_j \Vert^2} + (\Vert p_i - p_j \Vert - L_{ij}) \left(\frac{\mathbf{I}}{\Vert p_i - p_j \Vert} - \frac{(p_i - p_j) (p_i - p_j)^T}{\Vert p_i - p_j \Vert^3} \right) \right)
 */
-void compute_spring_force(const Eigen::VectorXd &q, const Eigen::VectorXd &qprev, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
+void compute_spring_force(const Eigen::VectorXd &q, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
 {
-    if (params_.springsEnabled)
+
+    std::vector<Eigen::Triplet<double>> triplet_list;
+    triplet_list.reserve(2 * particles_.size());
+
+    for (uint i = 0; i < connectors_.size(); i++)
     {
-        std::vector<Eigen::Triplet<double>> triplet_list;
-        triplet_list.reserve(2 * particles_.size());
+        double L = connectors_[i]->restlen;
+        double k = connectors_[i]->stiffness / L;
 
-        for (uint i = 0; i < connectors_.size(); i++)
+        int p1_index = connectors_[i]->p1;
+        int p2_index = connectors_[i]->p2;
+
+        Eigen::Vector2d pos1 = q.segment(2 * p1_index, 2);
+        Eigen::Vector2d pos2 = q.segment(2 * p2_index, 2);
+
+        double dist = (pos1 - pos2).norm();
+
+        Eigen::Vector2d pos_unit = (pos1 - pos2) / dist;
+
+        // Local Hessian matrix
+        Eigen::Matrix2d H_value = pos_unit * pos_unit.transpose() + (dist - L) * (Eigen::MatrixXd::Identity(2, 2) / dist - pos_unit * pos_unit.transpose() / dist);
+
+        // Local force vector
+        Eigen::VectorXd F_value = k * (dist - L) * pos_unit;
+
+        if (!particles_[p1_index].fixed)
         {
-            double L = connectors_[i]->restlen;
-            double k = connectors_[i]->stiffness / L;
+            // Corresponding index in the force vector
+            F.segment(2 * p1_index, 2) += -F_value;
 
-            int p1_index = connectors_[i]->p1;
-            int p2_index = connectors_[i]->p2;
-
-            Eigen::Vector2d pos1 = q.segment(2 * p1_index, 2);
-            Eigen::Vector2d pos2 = q.segment(2 * p2_index, 2);
-
-            double dist = (pos1 - pos2).norm();
-
-            Eigen::Vector2d pos_unit = (pos1 - pos2) / dist;
-
-            // Local Hessian matrix
-            Eigen::Matrix2d H_value = pos_unit * pos_unit.transpose() + (dist - L) * (Eigen::MatrixXd::Identity(2, 2) / dist - pos_unit * pos_unit.transpose() / dist);
-
-            // Local force vector
-            Eigen::VectorXd F_value = k * (dist - L) * pos_unit;
-
-            if (!particles_[p1_index].fixed)
-            {
-                // Corresponding index in the force vector
-                F.segment(2 * p1_index, 2) += -F_value;
-
-                // Corresponding index in the Hessian matrix
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index, 2 * p1_index, -H_value(0, 0)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index + 1, 2 * p1_index, -H_value(1, 0)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index, 2 * p1_index + 1, -H_value(0, 1)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index + 1, 2 * p1_index + 1, -H_value(1, 1)));
-            }
-            if (!particles_[p2_index].fixed)
-            {
-                F.segment(2 * p2_index, 2) += F_value;
-
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index, 2 * p2_index, H_value(0, 0)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index + 1, 2 * p2_index, H_value(1, 0)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index, 2 * p2_index + 1, H_value(0, 1)));
-                triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index + 1, 2 * p2_index + 1, H_value(1, 1)));
-            }
+            // Corresponding index in the Hessian matrix
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index, 2 * p1_index, -H_value(0, 0)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index + 1, 2 * p1_index, -H_value(1, 0)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index, 2 * p1_index + 1, -H_value(0, 1)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p1_index + 1, 2 * p1_index + 1, -H_value(1, 1)));
         }
+        if (!particles_[p2_index].fixed)
+        {
+            F.segment(2 * p2_index, 2) += F_value;
 
-        H.setFromTriplets(triplet_list.begin(), triplet_list.end());
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index, 2 * p2_index, H_value(0, 0)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index + 1, 2 * p2_index, H_value(1, 0)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index, 2 * p2_index + 1, H_value(0, 1)));
+            triplet_list.push_back(Eigen::Triplet<double>(2 * p2_index + 1, 2 * p2_index + 1, H_value(1, 1)));
+        }
+    }
+
+    H.setFromTriplets(triplet_list.begin(), triplet_list.end());
+}
+
+/*
+    Add the normal force due to the floor
+*/
+void floor_force(Eigen::VectorXd &q, Eigen::VectorXd &qdot, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
+{
+    for (uint i = 0; i < particles_.size(); i++)
+    {
+        if (particles_[i].pos(1) <= -0.5 && !particles_[i].fixed)
+        {
+            F(2 * i) += 0;
+            F(2 * i + 1) = 0;
+
+            q(2 * i + 1) = -0.5;
+            qdot(2 * i + 1) = 0;
+        }
     }
 }
 
-void computeForceAndHessian(const Eigen::VectorXd &q, const Eigen::VectorXd &qprev, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
+void computeForceAndHessian(Eigen::VectorXd &q, Eigen::VectorXd &qprev, Eigen::VectorXd &qdot, Eigen::VectorXd &F, Eigen::SparseMatrix<double> &H)
 {
     // Compute the total force and Hessian for all potentials in the system
     // This function should respect the booleans in params_ to allow the user
@@ -119,8 +133,18 @@ void computeForceAndHessian(const Eigen::VectorXd &q, const Eigen::VectorXd &qpr
     F.setZero();
     H.setZero();
 
-    compute_gravity_force(q, qprev, F, H);
-    compute_spring_force(q, qprev, F, H);
+    if (params_.gravityEnabled)
+    {
+        compute_gravity_force(q, F, H);
+    }
+    if (params_.springsEnabled)
+    {
+        compute_spring_force(q, F, H);
+    }
+    if (params_.floorEnabled)
+    {
+        floor_force(q, qdot, F, H);
+    }
 }
 
 Eigen::VectorXd newton_method(std::function<Eigen::VectorXd(Eigen::VectorXd)> func,
@@ -180,7 +204,7 @@ void numericalIntegration(Eigen::VectorXd &q, Eigen::VectorXd &qprev, Eigen::Vec
     {
     case SimParameters::TI_EXPLICIT_EULER:
     {
-        computeForceAndHessian(q, qprev, F, H);
+        computeForceAndHessian(q, qprev, qdot, F, H);
         qprev = q;
         q = q + params_.timeStep * qdot;
         qdot = qdot + params_.timeStep * Minv * F;
@@ -195,13 +219,13 @@ void numericalIntegration(Eigen::VectorXd &q, Eigen::VectorXd &qprev, Eigen::Vec
         // Lambda function for the implicit Euler method and its derivative
         auto implicit_euler = [&](Eigen::VectorXd q)
         {
-            computeForceAndHessian(q, qprev, F, H);
+            computeForceAndHessian(q, qprev, qdot, F, H);
             return q - qprev - params_.timeStep * (qdot + params_.timeStep * Minv * F);
         };
 
         auto implicit_euler_deriv = [&](Eigen::VectorXd q)
         {
-            computeForceAndHessian(q, qprev, F, H);
+            computeForceAndHessian(q, qprev, qdot, F, H);
             Eigen::SparseMatrix<double> sparse_I = Eigen::MatrixXd::Identity(2 * particles_.size(), 2 * particles_.size()).sparseView();
             Eigen::SparseMatrix<double> deriv = sparse_I - pow(params_.timeStep, 2) * Minv * H;
             return deriv;
@@ -216,16 +240,17 @@ void numericalIntegration(Eigen::VectorXd &q, Eigen::VectorXd &qprev, Eigen::Vec
     case SimParameters::TI_IMPLICIT_MIDPOINT:
     {
         qprev = q;
+        Eigen::VectorXd avg_q = (q + qprev) / 2;
         // Lambda function for the implicit midpoint method and its derivative
         auto implicit_midpoint = [&](Eigen::VectorXd q)
         {
-            computeForceAndHessian((q + qprev) / 2, qprev, F, H);
+            computeForceAndHessian(avg_q, qprev, qdot, F, H);
             return q - qprev - params_.timeStep * Minv * (2 * qdot + params_.timeStep * F) / 2;
         };
 
         auto implicit_midpoint_deriv = [&](Eigen::VectorXd q)
         {
-            computeForceAndHessian((q + qprev) / 2, qprev, F, H);
+            computeForceAndHessian(avg_q, qprev, qdot, F, H);
             Eigen::SparseMatrix<double> sparse_I = Eigen::MatrixXd::Identity(2 * particles_.size(), 2 * particles_.size()).sparseView();
             return sparse_I - 0.5 * pow(params_.timeStep, 2) * Minv * H;
         };
@@ -240,7 +265,7 @@ void numericalIntegration(Eigen::VectorXd &q, Eigen::VectorXd &qprev, Eigen::Vec
     {
         qprev = q;
         q = q + params_.timeStep * qdot;
-        computeForceAndHessian(q, qprev, F, H);
+        computeForceAndHessian(q, qprev, qdot, F, H);
         qdot = qdot + params_.timeStep * Minv * F;
 
         std::cout << "One step of Velocity Verlet" << std::endl;
