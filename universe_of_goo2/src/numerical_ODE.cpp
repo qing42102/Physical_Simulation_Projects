3b759f458cf09c75af9cf04a16bca0bfd88380f5
qing42102 <qing42102@outlook.com>
2024-03-06 19:45:02 -0600

    Step and project method. Doesn't work.

--- universe_of_goo2/src/numerical_ODE.cpp
+++ universe_of_goo2/src/numerical_ODE.cpp
@@ -72,6 +72,47 @@ Eigen::VectorXd newton_method(std::function<Eigen::VectorXd(Eigen::VectorXd)> fu
     return x;
 }
 
+Eigen::SparseMatrix<double> compute_constraint_deriv(const Eigen::VectorXd &q, const int &num_rigid_rods)
+{
+    Eigen::SparseMatrix<double> constrain_deriv(num_rigid_rods, q.size());
+    std::vector<Eigen::Triplet<double>> triplet_list;
+    for (uint i = 0; i < connectors_.size(); i++)
+    {
+        if (connectors_[i]->getType() == SimParameters::CT_RIGIDROD)
+        {
+            RigidRod *rod = dynamic_cast<RigidRod *>(connectors_[i]);
+            Eigen::Vector2d pos1 = q.segment(2 * rod->p1, 2);
+            Eigen::Vector2d pos2 = q.segment(2 * rod->p2, 2);
+
+            triplet_list.push_back(Eigen::Triplet<double>(i, 2 * rod->p1, 2 * (pos1 - pos2)[0]));
+            triplet_list.push_back(Eigen::Triplet<double>(i, 2 * rod->p1 + 1, 2 * (pos1 - pos2)[1]));
+
+            triplet_list.push_back(Eigen::Triplet<double>(i, 2 * rod->p2, -2 * (pos1 - pos2)[0]));
+            triplet_list.push_back(Eigen::Triplet<double>(i, 2 * rod->p2 + 1, -2 * (pos1 - pos2)[1]));
+        }
+    }
+    constrain_deriv.setFromTriplets(triplet_list.begin(), triplet_list.end());
+
+    return constrain_deriv;
+}
+
+Eigen::VectorXd compute_constraint(const Eigen::VectorXd &q, const int &num_rigid_rods)
+{
+    Eigen::VectorXd constraint(num_rigid_rods);
+    for (uint i = 0; i < connectors_.size(); i++)
+    {
+        if (connectors_[i]->getType() == SimParameters::CT_RIGIDROD)
+        {
+            RigidRod *rod = dynamic_cast<RigidRod *>(connectors_[i]);
+            Eigen::Vector2d pos1 = q.segment(2 * rod->p1, 2);
+            Eigen::Vector2d pos2 = q.segment(2 * rod->p2, 2);
+
+            constraint(i) = (pos1 - pos2).squaredNorm() - pow(rod->length, 2);
+        }
+    }
+    return constraint;
+}
+
 void penalty_method(const Eigen::VectorXd &q, Eigen::VectorXd &F)
 {
     for (uint i = 0; i < connectors_.size(); i++)
@@ -93,8 +134,104 @@ void penalty_method(const Eigen::VectorXd &q, Eigen::VectorXd &F)
     }
 }
 
-void step_project_method()
+void step_project_method(const Eigen::VectorXd &unconstrain_q,
+                         const Eigen::VectorXd &unconstrain_qdot,
+                         const Eigen::SparseMatrix<double> &Minv,
+                         Eigen::VectorXd &constrain_q,
+                         Eigen::VectorXd &constrain_qdot)
 {
+    int num_rigid_rods = 0;
+    for (uint i = 0; i < connectors_.size(); i++)
+    {
+        if (connectors_[i]->getType() == SimParameters::CT_RIGIDROD)
+        {
+            num_rigid_rods++;
+        }
+    }
+
+    Eigen::SparseMatrix<double> Minv_modified = Minv;
+    for (uint i = 0; i < particles_.size(); i++)
+    {
+        if (particles_[i].fixed)
+        {
+            Minv_modified.coeffRef(2 * i, 2 * i) = 0;
+        }
+    }
+
+    // Lambda function for the step and project method and its derivative
+    auto func = [&](Eigen::VectorXd state) -> Eigen::VectorXd
+    {
+        Eigen::VectorXd q = state.segment(0, 2 * particles_.size());
+        Eigen::VectorXd lambda = state.segment(2 * particles_.size(), num_rigid_rods);
+
+        Eigen::VectorXd constraint = compute_constraint(q, num_rigid_rods);
+        Eigen::SparseMatrix<double> constraint_deriv = compute_constraint_deriv(q, num_rigid_rods);
+
+        Eigen::VectorXd constraint_multiplier;
+        constraint_multiplier.resize(q.size());
+        constraint_multiplier.setZero();
+        for (uint i = 0; i < connectors_.size(); i++)
+        {
+            if (connectors_[i]->getType() == SimParameters::CT_RIGIDROD)
+            {
+                RigidRod *rod = dynamic_cast<RigidRod *>(connectors_[i]);
+
+                constraint_multiplier.segment(2 * rod->p1, 2) += lambda(i) * constraint_deriv.block(i, 2 * rod->p1, 1, 2).transpose();
+                constraint_multiplier.segment(2 * rod->p2, 2) += lambda(i) * constraint_deriv.block(i, 2 * rod->p2, 1, 2).transpose();
+            }
+        }
+        constraint_multiplier = Minv_modified * constraint_multiplier;
+
+        Eigen::VectorXd f(q.size() + num_rigid_rods);
+        f << (q - unconstrain_q) + constraint_multiplier, constraint;
+        return f;
+    };
+
+    auto deriv = [&](Eigen::VectorXd state) -> Eigen::SparseMatrix<double>
+    {
+        Eigen::VectorXd q = state.segment(0, 2 * particles_.size());
+        Eigen::VectorXd lambda = state.segment(2 * particles_.size(), num_rigid_rods);
+
+        Eigen::SparseMatrix<double> constraint_deriv = compute_constraint_deriv(q, num_rigid_rods);
+
+        Eigen::SparseMatrix<double> sparse_I = Eigen::MatrixXd::Identity(q.size(), q.size()).sparseView();
+
+        std::vector<Eigen::Triplet<double>> triplet_list;
+        for (uint i = 0; i < connectors_.size(); i++)
+        {
+            if (connectors_[i]->getType() == SimParameters::CT_RIGIDROD)
+            {
+                RigidRod *rod = dynamic_cast<RigidRod *>(connectors_[i]);
+
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p1, 2 * rod->p1, 1 + 2 * lambda(i)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p1 + 1, 2 * rod->p1 + 1, 2 * lambda(i)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p2, 2 * rod->p2, 1 - 2 * lambda(i)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p2 + 1, 2 * rod->p2 + 1, -2 * lambda(i)));
+
+                triplet_list.push_back(Eigen::Triplet<double>(q.size() + i, 2 * rod->p1, Minv_modified.coeff(2 * rod->p1, 2 * rod->p1) * constraint_deriv.coeff(i, 2 * rod->p1)));
+                triplet_list.push_back(Eigen::Triplet<double>(q.size() + i, 2 * rod->p1 + 1, Minv_modified.coeff(2 * rod->p1 + 1, 2 * rod->p1 + 1) * constraint_deriv.coeff(i, 2 * rod->p1 + 1)));
+                triplet_list.push_back(Eigen::Triplet<double>(q.size() + i, 2 * rod->p2, Minv_modified.coeff(2 * rod->p2, 2 * rod->p2) * constraint_deriv.coeff(i, 2 * rod->p2)));
+                triplet_list.push_back(Eigen::Triplet<double>(q.size() + i, 2 * rod->p2 + 1, Minv_modified.coeff(2 * rod->p2 + 1, 2 * rod->p2 + 1) * constraint_deriv.coeff(i, 2 * rod->p2 + 1)));
+
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p1, q.size() + i, constraint_deriv.coeff(i, 2 * rod->p1)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p1 + 1, q.size() + i, constraint_deriv.coeff(i, 2 * rod->p1 + 1)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p2, q.size() + i, constraint_deriv.coeff(i, 2 * rod->p2)));
+                triplet_list.push_back(Eigen::Triplet<double>(2 * rod->p2 + 1, q.size() + i, constraint_deriv.coeff(i, 2 * rod->p2 + 1)));
+            }
+        }
+        Eigen::SparseMatrix<double> df(q.size() + num_rigid_rods, q.size() + num_rigid_rods);
+        df.setFromTriplets(triplet_list.begin(), triplet_list.end());
+
+        return df;
+    };
+
+    Eigen::VectorXd initial_state(2 * particles_.size() + num_rigid_rods);
+    initial_state.segment(0, 2 * particles_.size()) = unconstrain_q;
+    initial_state.segment(2 * particles_.size(), num_rigid_rods) = Eigen::VectorXd::Zero(num_rigid_rods);
+
+    Eigen::VectorXd state = newton_method(func, deriv, initial_state);
+    constrain_q = state.segment(0, 2 * particles_.size());
+    constrain_qdot = unconstrain_qdot + (constrain_q - unconstrain_q) / params_.timeStep;
 }
 
 void lagrange_multiplier_method()
@@ -127,7 +264,10 @@ void numericalIntegration(Eigen::VectorXd &q, Eigen::VectorXd &lambda, Eigen::Ve
     case SimParameters::CH_STEPPROJECT:
     {
         qdot += params_.timeStep * Minv * F;
-        step_project_method();
+
+        Eigen::VectorXd constrain_q;
+        Eigen::VectorXd constrain_qdot;
+        step_project_method(q, qdot, Minv, constrain_q, constrain_qdot);
         break;
     }
 
