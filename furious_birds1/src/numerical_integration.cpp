60956e7eddccd42bd799a518870d9b8cc223de77
qing42102 <qing42102@outlook.com>
2024-03-28 17:59:52 -0500

    Add numerical integration for translational position and velocity, and angle

--- furious_birds1/src/numerical_integration.cpp
+++ furious_birds1/src/numerical_integration.cpp
@@ -8,6 +8,19 @@
 #include "SimParameters.h"
 #include "RigidBodyInstance.h"
 #include "RigidBodyTemplate.h"
+#include "VectorMath.h"
+
+void computeMassInverse(Eigen::DiagonalMatrix<double, Eigen::Dynamic> &Minv)
+{
+    Minv.resize(3 * bodies_.size());
+
+    for (uint i = 0; i < bodies_.size(); i++)
+    {
+        float mass = bodies_[i]->getTemplate().getVolume() * bodies_[i]->density;
+
+        Minv.diagonal().segment(3 * i, 3) = Eigen::Vector3d::Constant(1.0 / mass);
+    }
+}
 
 /*
     Newton's method for solving a system of nonlinear equations
@@ -55,13 +68,40 @@ Eigen::VectorXd newton_method(std::function<Eigen::VectorXd(Eigen::VectorXd)> fu
     return x;
 }
 
+/*
+    Compute the update for the angle using the formula
+    \text{rot} (\theta^i) \text{rot} (h \omega^i) = \text{rot} (\theta^{i+1})
+*/
+void update_angle(Eigen::VectorXd &angle,
+                  Eigen::VectorXd &angle_vel)
+{
+    for (uint i = 0; i < bodies_.size(); i++)
+    {
+        Eigen::Vector3d angle_i = angle.segment(3 * i, 3);
+        Eigen::Vector3d angle_vel_i = angle_vel.segment(3 * i, 3);
+
+        Eigen::Matrix3d rotation_mat = VectorMath::rotationMatrix(angle_i);
+        Eigen::Matrix3d rotation_mat_change = VectorMath::rotationMatrix(params_.timeStep * angle_vel_i);
+        Eigen::Vector3d new_angle = VectorMath::axisAngle(rotation_mat * rotation_mat_change);
+
+        angle.segment(3 * i, 3) = new_angle;
+    }
+}
+
 void numericalIntegration(Eigen::VectorXd &trans_pos,
                           Eigen::VectorXd &trans_vel,
                           Eigen::VectorXd &angle,
                           Eigen::VectorXd &angle_vel)
 {
+    trans_pos = trans_pos + params_.timeStep * trans_vel;
+
+    update_angle(angle, angle_vel);
+
     Eigen::VectorXd F;
     Eigen::SparseMatrix<double> H;
-
     computeForceAndHessian(trans_pos, trans_vel, angle, angle_vel, F, H);
+
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> Minv;
+    computeMassInverse(Minv);
+    trans_vel = trans_vel - Minv * params_.timeStep * F;
 }
